221839f26f95cc2f0335a8273b93bd32e1daf3c3
xb <852353298@qq.com>
2017-10-22 05:46:05 +0800

    python 大小写转换

---
+++ py-base/py003_string_text_transform.py
@@ -0,0 +1,10 @@
+# 字符串 大小写转换
+name = "ada lovelace"
+# titile() 首字母大写
+print(name.title())  # Ada Lovelace
+
+# upper()   全部大写
+print(name.upper());  # ADA LOVELACE
+
+# lower()   全部小写
+print(name.lower());  # ada lovelace
