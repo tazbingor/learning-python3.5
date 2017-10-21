5c1d7162b079c7699a639cef866f77327f373ab9
xb <852353298@qq.com>
2017-10-22 06:25:32 +0800

    python 常见字符串错误

---
+++ py-base/py006_string_errors_with_strings.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 17-10-22 上午6:20
+# @Author  : Aries
+# @Site    : 
+# @File    : py006_string_errors_with_strings.py
+# @Software: PyCharm
+
+# python 字符串中常见的错误操作
+# 正确的使用单引号和双引号
+# 正确写法
+message = "One of Python's strengths is its diverse community."
+print(message + "\n")
+# 错误写法
+# message = 'One of Python's strengths is its diverse community.'
+# print(message) # SyntaxError: invalid syntax
