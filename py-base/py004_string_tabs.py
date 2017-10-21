3c5fb67c16b2e6d3a98db1e2b2bf2074f22cbbca
xb <852353298@qq.com>
2017-10-22 06:00:46 +0800

    python 换行符和空白符

---
+++ py-base/py004_string_tabs.py
@@ -0,0 +1,10 @@
+#制表符和换行符
+
+#空白符
+print("人生苦短\t我用python\n")#人生苦短	我用python
+
+#换行符
+print("Languages:\nPython\nC\nJavaScript\n")
+
+#练习,使用一个单行字符串来生成四行输出
+print("Languages:\n\tPython\n\tJavaScript\n\tGo")
