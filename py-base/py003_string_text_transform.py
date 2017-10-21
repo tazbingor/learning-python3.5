66824d58961a6dcacbe9855ad8c63de62560497b
xb <852353298@qq.com>
2017-10-22 05:48:02 +0800

    python 字符串拼接

--- py-base/py003_string_text_transform.py
+++ py-base/py003_string_text_transform.py
@@ -8,3 +8,9 @@ print(name.upper());  # ADA LOVELACE
 
 # lower()   全部小写
 print(name.lower());  # ada lovelace
+
+# 合并字符串
+first_name = "ada"
+last_name = "lovelace"
+full_name = first_name + " " + last_name
+print(full_name)  # ada lovelace
