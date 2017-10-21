62e5058007f17298ffcd0d5ddd3674382b70182e
xb <852353298@qq.com>
2017-10-22 06:36:01 +0800

    python 引号转义

--- py-base/py006_string_errors_with_strings.py
+++ py-base/py006_string_errors_with_strings.py
@@ -14,3 +14,7 @@ print(message + "\n")
 # 错误写法
 # message = 'One of Python's strengths is its diverse community.'
 # print(message) # SyntaxError: invalid syntax
+
+#如何在字符串中表示单引号和双引号？
+print("余则成说：\"把茶叶交给可浓同志.!\"")
+print("余则成强调道：\'一定要把把茶叶交给可浓同志.!\'")
