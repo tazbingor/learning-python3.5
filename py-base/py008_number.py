288278f4ab7adfea944f206d9108fe322e8e5d08
jieyan <852353298@qq.com>
2017-10-23 12:19:33 +0800

    python number

---
+++ py-base/py008_number.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 17/10/23 下午12:11
+# @Author  : Aries
+# @Site    : 
+# @File    : py008_number.py
+# @Software: PyCharm
+# 数字
+
+# 加减乘除
+print(1+2)
+print(3-2)
+print(8*8)
+print(8/1) # 默认返回浮点数
+
