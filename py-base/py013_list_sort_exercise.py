e3233f2b18a148a5236db38d5d214c116cd7aa93
jieyan <852353298@qq.com>
2017-10-24 10:35:36 +0800

    python list sort exercise

---
+++ py-base/py013_list_sort_exercise.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 17/10/24 上午10:15
+# @Author  : Aries
+# @Site    : 
+# @File    : py013_list_sort_exercise.py
+# @Software: PyCharm
+# list 排序练习
+
+# 3-8 放眼世界:想出至少 5 个你渴望去旅游的地方。
+# 将这些地方存储在一个列表中,并确保其中的元素不是按字母顺序排列的。
+# 按原始排列顺序打印该列表。不要考虑输出是否整洁的问题,只管打印原始 Python 列表。
+# 使用 sorted()按字母顺序打印这个列表,同时不要修改它。
+# 再次打印该列表,核实排列顺序未变。
+# 使用 sorted()按与字母顺序相反的顺序打印这个列表,同时不要修改它。
+# 再次打印该列表,核实排列顺序未变。
+# 使用 reverse()修改列表元素的排列顺序。打印该列表,核实排列顺序确实变了。
+# 使用 reverse()再次修改列表元素的排列顺序。打印该列表,核实已恢复到原来的排列顺序。
+# 使用 sort()修改该列表,使其元素按字母顺序排列。打印该列表,核实排列顺序确实变了。
+# 使用 sort()修改该列表,使其元素按与字母顺序相反的顺序排列。打印该列表,核实排列顺序确实变了。
+
+tour = ['Xinjiang', 'Xizang', 'HongKong', 'Thailand', 'Australian']
+print(tour)
+print(sorted(tour))
+print(tour)
+print(sorted(tour, reverse=True))
+print(tour)
+tour.reverse()
+print(tour)
+tour.reverse()
+print(tour)
+
+tour.sort()
+print(tour)
+tour.sort(reverse=True)
+print(tour)
