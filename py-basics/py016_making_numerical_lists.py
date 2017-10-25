9197c53d55beb0ad58f92421591ba996f43ce1d3
jieyan <852353298@qq.com>
2017-10-25 16:10:10 +0800

    python lists exercise

---
+++ py-basics/py016_making_numerical_lists.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 17/10/24 上午11:29
+# @Author  : Aries
+# @Site    : 
+# @File    : py016_making_numerical_lists.py
+# @Software: PyCharm
+# 创建数值列表
+
+# 打印1-5之间的数字
+for num in range(1, 6):
+    print(num)
+print("\n")
+
+# range 指定步长
+# 打印1~10内的偶数
+even_number = list(range(2, 11, 2))
+print(even_number)
