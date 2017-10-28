248e8928d435f09173b60688ef2b881eace7e28a
jieyan <852353298@qq.com>
2017-10-28 08:33:54 +0800

    python hello world

--- py-basics/py001_hello_python.py
+++ py-basics/py001_hello_python.py
@@ -1,8 +1,10 @@
 # print("python切换版本")
 # 变量
-num = 15
-num +=15
-print(num)
-
-str = "python"
-print(str)
+# num = 15
+# num +=15
+# print(num)
+#
+# str = "python"
+# print(str)
+print('\n')
+print("hello world")
--- py-basics/py026_if_lists_exercise.py
+++ py-basics/py026_if_lists_exercise.py
@@ -5,3 +5,14 @@
 # @Site    : 
 # @File    : py026_if_lists_exercise.py
 # @Software: PyCharm
+
+# 5-8 以特殊方式跟管理员打招呼:创建一个至少包含 5 个用户名的列表,且其中一个用户名为'admin'。
+# 想象你要编写代码,在每位用户登录网站后都打印一条问候消息。 遍历用户名列表,并向每位用户打印一条问候消息。
+# 如果用户名为'admin',就打印一条特殊的问候消息,如“Hello admin, would you like to see a status report?”。
+# 否则,打印一条普通的问候消息,如“Hello Eric, thank you for logging in again”。
+users = ['admin', 'billy', 'ruby', 'joker', 'woody']
+
+
+# 5-9 处理没有用户的情形:在为完成练习 5-8 编写的程序中,添加一条 if 语句,检 查用户名列表是否为空。
+# 如果为空,就打印消息“We need to find some users!”。 15
+# 删除列表中的所有用户名,确定将打印正确的消息。
