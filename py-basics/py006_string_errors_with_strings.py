1073657d13768b2ac312861cefca7b3b35173754
jieyan <852353298@qq.com>
2017-11-13 16:57:08 +0800

    python 3.5 string

--- py-basics/py006_string_errors_with_strings.py
+++ py-basics/py006_string_errors_with_strings.py
@@ -15,6 +15,14 @@ print(message + "\n")
 # message = 'One of Python's strengths is its diverse community.'
 # print(message) # SyntaxError: invalid syntax
 
-#如何在字符串中表示单引号和双引号？
+# 如何在字符串中表示单引号和双引号？
 print("余则成说：\"把茶叶交给可浓同志.!\"")
 print("余则成强调道：\'一定要把把茶叶交给可浓同志.!\'")
+
+'''
+注意,python3.5和python2.7中字符串的区别,其中一条就是3.5中没有unicode 的type,只有
+'''
+str_u = u'python 3.5'
+print(type(str_u))
+str = str(str_u)
+print(type(str))
