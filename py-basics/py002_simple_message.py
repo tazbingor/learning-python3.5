e10cb46fd1b9371c3c0c8ee0bf785a5ede56c519
jieyan <852353298@qq.com>
2017-11-26 22:50:57 +0800

    python 3.5 string

--- py-basics/py002_simple_message.py
+++ py-basics/py002_simple_message.py
@@ -1,6 +1,6 @@
 #2-1 简单消息:将一条消息存储到变量中,再将其打印出来。
 #2-2 多条简单消息:将一条消息存储到变量中,将其打印出来;再将变量的值修改 为一条新消息,并将其打印出来。
-message = "把茶叶交给克农同志！"
+message = "python"
 print(message)
-message = "记住！一定要把茶叶交给克农同志！"
+message = "python web"
 print(message)
